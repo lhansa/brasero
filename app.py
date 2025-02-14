e4e7e9a55fb0a1c1a5f2bdb1abc9f03a6a37bd42
Leonardo Hansa <hansa.leonardo@gmail.com>
2025-02-14 20:43:37 +0000

    telegram skeleton

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+def send_telegram(MENSAJE):
+
+    TOKEN = os.environ['TELEGRAM_TOKEN']
+    CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+
+    full_url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    params = {
+        "chat_id": CHAT_ID,
+        "text": MENSAJE
+    }
+
+    try:
+        with requests.post(full_url, data=params) as response:
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"message": "Mensaje enviado correctamente a Telegram"})
+            }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
